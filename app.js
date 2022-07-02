7cf9cb0f919e5418d50e4791e7db22dd32884873
HelisonOliveira <helisonoliveira@hotmail.com>
2022-07-02 16:18:08 -0300

    first Commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const calc = require("./calculadora")
+
+calc.printResult(calc.sum(5,10))
+calc.printResult ("helison")
---
+++ calculadora.js
@@ -0,0 +1,14 @@
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function mult(num1, num2) {
+    return num1 * num2
+}
+
+function printResult(text) {
+    console.log(text)
+}
+
+module.exports = { sum, mult, printResult }
